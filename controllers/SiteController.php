f66a2972106b374788941cbf1f497799eedad017
Marina Zayceva <marinazayceva89@gmail.com>
2017-03-15 19:32:57 +0500

    fixed redirect

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,13 +4,17 @@
 use Faker\Provider\cs_CZ\DateTime;
 use Yii;
     use yii\filters\AccessControl;
-    use yii\web\Controller;
+use yii\httpclient\CurlTransport;
+use yii\web\Controller;
     use yii\filters\VerbFilter;
     use app\models\LoginForm;
     use app\models\ContactForm;
     use yii\httpclient\Client;
 
     class SiteController extends Controller {
+        private $accessToken = '';
+        private $contributorID = '2981518';
+
         /* other code */
         public function actionSpeak($message = "default message") {
             return $this->render("speak",['message' => $message]);
@@ -46,13 +50,9 @@ use Yii;
 
         public function actionAuthorize(){
             $client = new Client();
+            $redirect_uri = $this->getRedirectURI();
+            $redirect_uri .= '/index.php?r=site/client';
 
-            $redirect_uri = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
-            $redirect_uri .= "://{$_SERVER['SERVER_NAME']}";
-            if (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != 80) {
-                $redirect_uri .= ":{$_SERVER['SERVER_PORT']}";
-            }
-            $redirect_uri .= '/index.php';
             $response = $client->get('https://api.shutterstock.com/v2/oauth/authorize',['client_id' => '1fed276e29a88731787a',
                     'state' => '347yctr4bcfx4unzelmqwjfixz',
                     'scope' => 'user.view',
@@ -61,24 +61,69 @@ use Yii;
                 ->setHeaders(['User-Agent'=>'Yii2'])
                 ->send();
             $redirect = $response->getHeaders()['Location'];
-            header('Content-type: application/json');
-            var_dump($_GET['code']);
+
+            $this->redirect($redirect);
 
         }
 
         public function actionClient(){
             $client = new Client();
 
-            $response = $client->createRequest()
-                ->setMethod('post')
-                ->setUrl('https://api.shutterstock.com/v2/oauth/access_token')
-                ->setData(['client_id' => '1fed276e29a88731787a', 'client_secret' => '92fb4ce1e90d3747f2deb68134839d86ca88a206',
-                        'grant_type'=>'authorization_code'])
-                ->send();
-            var_dump($response->statusCode);
+            if (isset($_GET['code'])) {
+                $redirect_uri = $this->getRedirectURI();
+                $redirect_uri .= '/index.php?r=site/success';
+
+                $response = $client->createRequest()
+                    ->setMethod('post')
+                    ->setUrl('https://api.shutterstock.com/v2/oauth/access_token')
+                    ->setData(['client_id' => '1fed276e29a88731787a', 'client_secret' => '92fb4ce1e90d3747f2deb68134839d86ca88a206',
+                        'grant_type' => 'authorization_code', 'code' => $_GET['code'], 'redirect_uri' => $redirect_uri])
+                    ->setHeaders(['User-Agent'=>'Yii2'])
+                    ->send();
+
+                $this->accessToken = $response->getData()['access_token'];
+                //var_dump($this->accessToken);
+
+                $new_redirect_uri = $this->getRedirectURI();
+                $new_redirect_uri .= '/index.php?r=site/contributor';
+                //var_dump($new_redirect_uri);
+                $this->redirect($new_redirect_uri);
+            }
         }
 
+        public function actionSuccess(){
 
+            $redirect_uri = $this->getRedirectURI();
+            $redirect_uri .= '/index.php?r=site/contributor';
+            $this->redirect($redirect_uri);
+
+        }
+
+        private function getRedirectURI(){
+            $redirect_uri = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+            $redirect_uri .= "://{$_SERVER['SERVER_NAME']}";
+            if (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != 80) {
+                $redirect_uri .= ":{$_SERVER['SERVER_PORT']}";
+            }
+            return $redirect_uri;
+        }
+
+        public function actionContributor(){
+            $client = new Client();
+            //header('Authorization: Bearer v2/MWZlZDI3NmUyOWE4ODczMTc4N2EvMTcyODQ1ODE0L2N1c3RvbWVyLzIvTEc0ZVNHOUNpeFA2M01PRFUyQ0pPVGd5elB0WkJnZV82U1RhMVo2M1kxeW5ZTDRkUmZsUE5PVGJ3WG1NTklKdktiLUVXZXA3b1Z6WUFaN3ZmU2RhQTVfai1FNzhiVHNKNy01cnpwN3FUV1lJMzIwMnhTdlZycDBMVXF2ZmJTWjFqMXM0Q0Y5Rk1fek9KZUJjRkRBUjN4MzNYUFdoNl82TlJ4cWRZbTRaN3FUSExuR01CSDNfSXVCQTUxQUx1XzJz');
+            $request = $client->createRequest()
+                ->setMethod('get')
+                ->setUrl('https://api.shutterstock.com/v2/contributors/'.$this->contributorID)
+                ->setHeaders(['User-Agent'=>'Yii2','Authorization' => 'Bearer v2/MWZlZDI3NmUyOWE4ODczMTc4N2EvMTcyODQ1ODE0L2N1c3RvbWVyLzIvTEc0ZVNHOUNpeFA2M01PRFUyQ0pPVGd5elB0WkJnZV82U1RhMVo2M1kxeW5ZTDRkUmZsUE5PVGJ3WG1NTklKdktiLUVXZXA3b1Z6WUFaN3ZmU2RhQTVfai1FNzhiVHNKNy01cnpwN3FUV1lJMzIwMnhTdlZycDBMVXF2ZmJTWjFqMXM0Q0Y5Rk1fek9KZUJjRkRBUjN4MzNYUFdoNl82TlJ4cWRZbTRaN3FUSExuR01CSDNfSXVCQTUxQUx1XzJz']);
+
+            var_dump($request->getHeaders());
+            $response = $request->send();
+            var_dump($response->getContent());
+        }
+
+        public function actionInfo(){
+            var_dump($_SERVER);
+        }
 
 }
 ?>
